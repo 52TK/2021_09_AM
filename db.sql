190d632456762d4e939d975ef9edaf1199d6c72e
52TK <0910ydn@gmail.com>
2021-09-14 19:08:35 +0900

    DB 연결

---
+++ db.sql
@@ -0,0 +1,35 @@
+#DB 삭제/생성/사용
+DROP DATABASE IF EXISTS dy;
+CREATE DATABASE dy;
+USE dy;
+
+# 게시물 테이블 생성
+CREATE TABLE article(
+    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    regDate DATETIME NOT NULL,
+    title CHAR(255) NOT NULL,
+    `body` LONGTEXT NOT NULL
+);
+
+# 게시물 데이터 추가 
+INSERT INTO article 
+SET regDate = NOW(),
+title = '제목1',
+`body` = '내용1';
+
+INSERT INTO article 
+SET regDate = NOW(),
+title = '제목2',
+`body` = '내용2';
+
+INSERT INTO article 
+SET regDate = NOW(),
+title = '제목3',
+`body` = '내용3';
+
+INSERT INTO article 
+SET regDate = NOW(),
+title = '제목4',
+`body` = '내용4';
+
+SELECT * FROM article;
