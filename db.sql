f68b666bf34fc932a6b65e0fb4ed457672374664
52TK <0910ydn@gmail.com>
2021-09-29 17:38:33 +0900

    회원 테이블 생성

--- db.sql
+++ db.sql
@@ -11,6 +11,15 @@ CREATE TABLE article(
     `body` LONGTEXT NOT NULL
 );
 
+#회원 테이블 생성
+CREATE TABLE `member` (
+    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    regDate DATETIME NOT NULL,
+    loginId CHAR(100) NOT NULL UNIQUE,
+    loginPw CHAR(100) NOT NULL,
+    `name` CHAR(100) NOT NULL
+);
+
 # 게시물 데이터 추가 
 INSERT INTO article 
 SET regDate = NOW(),
