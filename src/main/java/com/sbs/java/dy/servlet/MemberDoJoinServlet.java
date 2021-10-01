f26448fabfd05fe21fd45b6a3f8c61b275c2093d
52TK <0910ydn@gmail.com>
2021-10-01 11:32:45 +0900

    회원가입시 로그인 아이디 중복체크

--- src/main/java/com/sbs/java/dy/servlet/MemberDoJoinServlet.java
+++ src/main/java/com/sbs/java/dy/servlet/MemberDoJoinServlet.java
@@ -44,8 +44,20 @@ public class MemberDoJoinServlet extends HttpServlet {
 			String loginId = request.getParameter("loginId");
 			String loginPw = request.getParameter("loginPw");
 			String name = request.getParameter("name");
+			
+			SecSql sql = SecSql.from("SELECT COUNT(*) AS cnt ");
+			sql.append("FROM `member`");
+			sql.append("WHERE loginId = ?", loginId);
 
-			SecSql sql = SecSql.from("INSERT INTO member");
+			boolean isJoinAvailableLoginId = DBUtil.selectRowIntValue(con, sql) == 0;
+
+			if(isJoinAvailableLoginId == false) {
+				response.getWriter().append(
+						String.format("<script> alert('%s (은)는 이미 사용중인 아이디 입니다.'); history.back(); </script>", loginId));
+				return;
+			}
+
+			sql = SecSql.from("INSERT INTO member");
 			sql.append("SET regDate = NOW()");
 			sql.append(", loginId = ?", loginId);
 			sql.append(", loginPw = ?", loginPw);
